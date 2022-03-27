8e0a015fd1646059bd0882c7b352f8bec91123cb
Zoey <jsisavath@gmail.com>
2022-03-27 04:02:17 -0500

    added questions to css array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,61 @@
 //array of questions and answers
-let arr= {
-    Question 1:"Commonly used data types do not include:"
-    answer: 
-}
+const myQuestions = [
+    {
+      question: "Commonly used data types DO Not Include_______?",
+      answers: {
+        1: "strings",
+        2: "booleans",
+        3: "alerts",
+        4: "numbers",
+
+      },
+      correctAnswer: 3
+    },
+    {
+      question: "The condition in an if / else statement is enclosed with _______?",
+      answers: {
+        1: "quotes",
+        2: "curly brackets",
+        3: "parenthesis",
+        4: "square brackets",
+
+      },
+      correctAnswer: 3
+    },
+    {
+      question: "Arrays in JavaScript can be used to store_______?",
+      answers: {
+        1: "numbers and strings",
+        2: "other arravs",
+        3: "booleans",
+        4: "all of the above",
+
+      },
+      correctAnswer:4
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis",
+            
+        },
+        correctAnswer:3
+      },
+      {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log",
+              
+        },
+        correctAnswer:3
+      },
+  ];
 
 //add event listener to start button 
 
