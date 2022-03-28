14a07792e3d99189576b6ac1e05a16177b173653
Zoey <jsisavath@gmail.com>
2022-03-28 02:42:02 -0500

    added click to each answer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,43 +60,69 @@ var myQuestions = [
 //variables declared 
 var score=0;
 var myQuestionsIndex=0;
-var count = 75;
+var count= 75;
 
 //constants declared
 var startPage = document.querySelector("#start-page");
 var quizBox = document.querySelector("#quiz-box");
-var timer = document.querySelector("header .timer");
 var timeCount = document.querySelector(".timer");
 var answerChoices = document.getElementById(".answer-choices");
-
+var startBtn= document.getElementById("#start-btn");
 
 //event click button starts the quiz
 function startQuiz (){
     console.log("quiz begins!");
-
-    var setQuestion=document.querySelector("#quiz-box");
-    setQuestion.textContent= myQuestions;
-    
-    //loop through each question and answer array and set to question-box
-    /*for(i=0;i<myQuestionsIndex.length;i++);{
-        function setQuestion ()
-    }*/
-
 //starting quiz starts timer
-var interval = setInterval(function(){
-  console.log("timer starts");
-  document.querySelector("header .timer").innerHTML=count;
-  count--;
-  if (count === 0){
-    clearInterval(interval);
-  }
-}, 1000);
+var timer = document.querySelector("header .timer");
+function countdown() {
+    var timeLeft = 75;
+    console.log ("timer starts");
+    
+    var timeInterval = setInterval(function () {
+    timer.textContent = timeLeft 
+    timeLeft--;
+        if (timeLeft===0){
+            console.log("Game over");
+            timer.textContent = '';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+countdown ();
+};
+//loads quiz questions on question box from array 
+const questionEl = document.querySelector("#question")
+questionEl.textContent = myQuestions[0].question
 
+//answers need IDs added to the li element, but this is an example of what it could look like after you add an ID
+const answer1El = document.querySelector("#answer1");
+answer1El.textContent = myQuestions[0].answers[1]
+const answer2El = document.querySelector("#answer2");
+answer2El.textContent = myQuestions[0].answers[2]
+const answer3El = document.querySelector("#answer3");
+answer3El.textContent = myQuestions[0].answers[3]
+const answer4El = document.querySelector("#answer4");
+answer4El.textContent = myQuestions[0].answers[4]
+
+//when we select a choice we check for true or false (if TRUE submit score, if FALSE reduce time)
+
+// the following line turns the HTMLCollection into a true JS array
+let nextQuestionBtns = new Array(...document.getElementsByClassName('answer-choices') );
+
+//attach listener to each choice button
+nextQuestionBtns.forEach( function(button) {
+  // here you can attach the listeners:
+  button.onclick = function(){
+      console.log("you clicked a choice!");
+    // and here, you would check if this button is contained by the appropriate question
+  }
+})
+
+if (nextQuestionBtns=== myQuestions[0].correctAnswer[0]){
+    console.log("that's right");
 };
 
 
-
-//when we select a choice we check for true or false (if TRUE submit score, if FALSE reduce time)
 /*var checkAnswer= function(event){
 console.log(event.trigger)
 };
@@ -117,14 +143,12 @@ function currentQuestion (){
 });*/
 
 //add event listener to start button 
-$( "#start-btn" ).click(function() {
+$("#start-btn").click(function () {
     console.log("you clicked it!");
-});
-var startBtn = document.querySelector("#start-btn");
-startBtn.addEventListener('click', startQuiz);
-$(document).ready(function() {
-    $("#start-btn").click(function() {
-        $("#start-page").remove();
-        $( "#quiz-box" ).show();
-    });
-});
+    $("#start-page").remove();
+    console.log("page stolen");
+    document.querySelector("#quiz-box").style.display = "block";
+    console.log("returned questions box!");
+    startQuiz();
+    console.log ("quiz started!");
+    })
